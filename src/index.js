f4fd9121aa26d95d5b60bc56dd9affb33b8dd61f
Ddejong87 <d.vangastel87@gmail.com>
2021-08-02 20:26:08 +0200

    adding description for weather

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ function displayTemperature(response){
     console.log(response.data.main.temp);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city")
+    let descriptionElement = document.querySelector("#description")
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
 
   
 }
