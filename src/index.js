63a02ad63dfd133a29fa9c46efd54f62e39f9db3
Ddejong87 <d.vangastel87@gmail.com>
2021-08-02 21:10:44 +0200

    updated the time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 function formatDate (timestamp){
     let date = new Date(timestamp);
     let hours = date.getHours();
+    if (hours <10) { hours = `0${hours}`};
     let minutes = date.getMinutes();
+    if (minutes <10) { minutes = `0${minutes}`};
+
     let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
