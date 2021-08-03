efcff289c1de9ee4aa1cf81d686024bf8c9f353a
Ddejong87 <d.vangastel87@gmail.com>
2021-08-03 22:16:09 +0200

    updated CSS

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
-    let precipitationElement = document.querySelector ("#precipitation");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector ("#date");
@@ -30,7 +29,6 @@ celsiusTemperature = response.data.main.temp;
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
-    precipitationElement.innerHTML = response.data
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round (response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000)
