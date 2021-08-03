a97219487051136b2076993bb08e83420cc31b7b
Ddejong87 <d.vangastel87@gmail.com>
2021-08-03 20:42:07 +0200

    form and searchfunction added
    created a form and changed to coding to have a search function in place

--- src/index.js
+++ src/index.js
@@ -29,13 +29,24 @@ function displayTemperature(response){
     iconElement.setAttribute ("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 iconElement.setAttribute ("alt", response.data.weather[0].description);
 
-  
 }
 
+function search(city){
 let apiKey = "bf1a450160d22fdfe731b236aa5f1569";
-let city = "tilburg";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
 axios.get(apiUrl).then(displayTemperature);
+}
 
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input")
+    search(cityInputElement.value);
+
+}
+
+
+search("Tilburg");
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
