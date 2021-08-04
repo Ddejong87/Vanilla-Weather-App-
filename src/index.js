b8a2adbaed35d8bece2cffaf902875579e3e1765
Ddejong87 <d.vangastel87@gmail.com>
2021-08-04 20:46:30 +0200

    added the forecast for multiple days into JS

--- src/index.js
+++ src/index.js
@@ -12,34 +12,25 @@ function formatDate (timestamp){
 }
 
 function displayForecast(){
-    let forecastElement = document.querySelector("#forecast");
+let forecastElement = document.querySelector("#forecast");
+
 let forecastHTML = `<div class= "row">`;
-forecastHTML = forecastHTML +  
-        `<div class= "col-2">
+let days = ["Thu", "Fri", "Sat", "Sun"];
+days.forEach(function(day){
+    forecastHTML = forecastHTML + 
+ `<div class= "col-2">
             <div class="weather-forecast-date">
-            thu
+            ${day}</div>
             <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="36px"/>
             <div class="weather-forecast-temperatures">
             <span class="weather-forecast-max">18°</span> 
             <span class="weather-forecast-min">12°</span> 
-        </div>
+            </div>
     </div>`;
-forecastHTML = forecastHTML +  
-        `<div class= "col-2">
-            <div class="weather-forecast-date">
-            thu
-            <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="36px"/>
-            <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-max">18°</span> 
-            <span class="weather-forecast-min">12°</span> 
-        </div>
-    </div>`;
-forecastHTML ='</div>';
 
-    forecastElement.innerHTML = forecastHTML
-
-   forecastElement.innerHTML =  `
-   
+});
+forecastHTML = forecastHTML +'</div>';
+forecastElement.innerHTML = forecastHTML
 } 
 
 function displayTemperature(response){
